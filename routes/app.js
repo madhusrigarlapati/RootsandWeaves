c93f7884834c874f8dca2cc2207b36fd271f48a7
madhusrigarlapati <20pa1a1234@vishnu.edu.in>
2023-02-27 21:45:23 +0530

    second commit

--- routes/app.js
+++ routes/app.js
@@ -4,7 +4,7 @@ const ejs=require('ejs');
 
 var mongojs = require('mongojs')
 var cString="mongodb+srv://madhu:madhu@cluster0.y34pj1v.mongodb.net/?retryWrites=true&w=majority"
-var db = mongojs(cString, ['customer','retailer','gros','yretailer'])
+var db = mongojs(cString, ['customer','retailer','gros','Yretailer','Zretailer','orders','accord'])
 
 router.post('/ccreate',async(req,res)=>{
     try{
@@ -34,6 +34,57 @@ router.post('/ccreate',async(req,res)=>{
     }
 })
 
+router.post('/order',async (req,res)=>{
+    try{
+        const id=req.body.cus;
+        const id1=req.body.bel;
+        //console.log(id1);
+        var UserJson={
+            custName:id,
+            prodName:req.body.pronam,
+            retName:req.body.rettname,
+            quantit:req.body.quanti
+        }
+        //console.log(UserJson);
+        if(req.body.bel=="veggies"){
+            db.orders.insert(UserJson,function(err,docs){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    //console.log(docs);
+                    res.render('order')
+                }
+            })
+        }
+        if(req.body.bel=="handloom"){
+            db.Yorders.insert(UserJson,function(err,docs){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    //console.log(docs);
+                    res.render('order')
+                }
+            })
+        }
+        if(req.body.bel=="pottrey"){
+            db.Zorders.insert(UserJson,function(err,docs){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    //console.log(docs);
+                    res.render('order')
+                }
+            })
+        }
+        
+    }
+    catch(err){
+        console.log(err);
+    }
+})
 
 router.post('/Yrcreate',async(req,res)=>{
     try{
@@ -157,13 +208,14 @@ router.post('/custSignIn1',async function(req,res){
 
 router.post('/retSignIn1',async function(req,res){
     var id1=req.body.rname
-    var password=req.body.rpass
+    var pwd=req.body.rpass
     //console.log(id);
     let test = db.retailer;
     let test1 = db.gros;
+    let test2=db.orders;
     //console.log(test);
     var l=new Array();
-    let promiseOfFind = test.find({name:id1}).toArray((err, docs) => {
+    let promiseOfFind = test.find({name:id1,password:pwd}).toArray((err, docs) => {
             if(docs.length!=0){
                 var a1= docs[0].name;
                 var a2= docs[0].phoneNo;
@@ -175,9 +227,12 @@ router.post('/retSignIn1',async function(req,res){
                     if(docs1.length==0){
                         s="no products added fill the form to add "
                     }
+                    let f1= test2.find({retName:id1}).toArray((err,docs2)=>{
+                        //console.log(docs2)
+                        res.render('retSignIn1',{Name:a1,phoneNo:a2,farmaddress:a3,l2:docs1,s:s,l3:docs2});
 
+                    })
                     //console.log(docs1);
-                    res.render('retSignIn1',{Name:a1,phoneNo:a2,farmaddress:a3,l2:docs1,s:s});
     
 
                 })
@@ -189,17 +244,127 @@ router.post('/retSignIn1',async function(req,res){
     })
         
 })
+router.post('/accorders',async (req,res)=>{
+    var id1=req.body.pp;
+    var id2=req.body.qq;
+    var id3=req.body.rr;
+    var id4=req.body.cc;
+    
+    var test=db.gros;
+    var test1=db.orders;
+    var test2=db.accord;
+    var test3=db.rejord;
+    let promiseOfFind = test.find({name:id3}).toArray((err, docs) => {
+        if(docs.length!=0){
+            console.log(docs);
+            for(var i=0;i<docs.length;i++){
+                if(docs[i].grname.l.prodname==id1){
+                    if(docs[i].grname.l.quantity>=id2){
+                        // docs[i].grname.l.quantity=docs[i].grname.l.quantity-id2;
+                        var a=docs[i].grname.l.quantity-id2;
+                        var a1=docs[i].grname.l.prodname;
+                        var a2=docs[i].grname.l.link;
+                        var a3=docs[i].grname.l.price;
+                        console.log(a);
+                        test.update({"name":id3},{$set:{grname:{l:{prodname:a1,quantity:a,link:a2,price:a3}}}})
+                        var user={
+                            custName:id4,
+                            prodName:id1,
+                            quantit:id2,
+                            retName:id3
+                        }
+                        test2.insert(user,function(err,docs){
+                            if(err){
+                                console.log(err);
+                            }
+                            else{
+                                console.log(docs);
+                            }
+                            test1.remove({custName:id4,prodName:id1,quantit:id2,retName:id3});
+                         })  
+                        res.send("Accepted order successfully")
+                    }
+                    // else if(docs[i].grname.l.quantity==id2){
+                    //     var a=docs[i].grname.l.quantity-id2;
+                    //     var a1=docs[i].grname.l.prodname;
+                    //     var a2=docs[i].grname.l.link;
+                    //     var a3=docs[i].grname.l.price;
+                    //     console.log(a);
+                    //     test.remove({name:id3,grname:{l:a1}})
+                    //     // test.update({"name":id3},{$set:{grname:{l:{prodname:a1,quantity:a,link:a2,price:a3}}}})
+                    //     var user={
+                    //         custName:id4,
+                    //         prodName:id1,
+                    //         quantit:id2,
+                    //         retName:id3
+                    //     }
+                    //     test2.insert(user,function(err,docs){
+                    //         if(err){
+                    //             console.log(err);
+                    //         }
+                    //         else{
+                    //             console.log(docs);
+                    //         }
+                    //         test1.remove({custName:id4,prodName:id1,quantit:id2,retName:id3});
+                    //     })  
+                    //     res.send("Accepted order successfully")
+                    // }
+                    else{
+                        var user={
+                            custName:id4,
+                            prodName:id1,
+                            quantit:id2,
+                            retName:id3
+                        }
+                        test3.insert(user,function(err,docs){
+                            if(err){
+                                console.log(err);
+                            }
+                            else{
+                                console.log(docs);
+                            }
+                            test1.remove({custName:id4,prodName:id1,quantit:id2,retName:id3});
+                         })
+                         res.send("rejected the order")
+                    }
+                }
+                console.log(docs[i].grname.l.quantity)
+            }
+            console.log(docs);
+            
+
+        }
+    })
+
+})
+router.post('/orderstat',async function(req,res){
+    var id1=req.body.cus1;
+    var test=db.accord;
+    var test1=db.rejord;
+    var test2=db.orders;
+    let promiseOfFind = test.find({custName:id1}).toArray((err, docs) => {
+        console.log(docs);
+        let ff = test1.find({custName:id1}).toArray((err, docs1) => {
+            console.log(docs1);
+                let ff1=test2.find({custName:id1}).toArray((err, docs2) => {
+                    console.log(docs2);
+                    res.render('orderstat',{accc:docs,rejj:docs1,pend:docs2})
+                })
+        })
+    })
+
+})
 
 
     router.post('/YretSignIn1',async function(req,res){
         var id1=req.body.y1rname
-        var password=req.body.y1rpass
+        var pwd=req.body.y1rpass
         console.log(id1);
         let test = db.Yretailer;
         let test1 = db.gros;
         //console.log(test);
         var l=new Array();
-        let promiseOfFind = test.find({name:id1}).toArray((err, docs) => {
+        let promiseOfFind = test.find({name:id1,password:pwd}).toArray((err, docs) => {
                 if(docs.length!=0){
                     var a1= docs[0].name;
                     var a2= docs[0].phoneNo;
@@ -229,13 +394,13 @@ router.post('/retSignIn1',async function(req,res){
 
         router.post('/ZretSignIn1',async function(req,res){
             var id1=req.body.z1rname
-            var password=req.body.z1rpass
+            var pwd=req.body.z1rpass
             //console.log(id1);
             let test = db.Zretailer;
             let test1 = db.gros;
             //console.log(test);
             var l=new Array();
-            let promiseOfFind = test.find({name:id1}).toArray((err, docs) => {
+            let promiseOfFind = test.find({name:id1,password:pwd}).toArray((err, docs) => {
                     if(docs.length!=0){
                         var a1= docs[0].name;
                         var a2= docs[0].phoneNo;
@@ -310,7 +475,60 @@ router.post('/retSignIn2',async function(req,res){
         //console.log(docs1.length);})
 })
 })
+router.post('/YretSignIn2',async function(req,res){
+    var id1=req.body.f11name;
+    let test1 = db.gros;
+    let f= test1.find({name:id1}).toArray((err,docs1)=>{
+            var grname=req.body.gname;
+            var l={
+                prodname:req.body.gname,
+                link:req.body.photo,
+                quantity:req.body.quant,
+                price:req.body.price
+            }
+            var UserJson={
+                name:id1,
+                grname:{l}
+            }
+            test1.insert(UserJson,function(err,docs2){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    //console.log(docs2);
+                    res.send("successfully added details");
+                }
+            })
+        })
+})
 
+router.post('/ZretSignIn2',async function(req,res){
+    var id1=req.body.f13name;
+    let test1 = db.gros;
+    let f= test1.find({name:id1}).toArray((err,docs1)=>{
+            var grname=req.body.gname3;
+            var l={
+                prodname:req.body.gname3,
+                link:req.body.photo3,
+                quantity:req.body.quant3,
+                price:req.body.price3
+            }
+            var UserJson={
+                name:id1,
+                grname:{l}
+            }
+            test1.insert(UserJson,function(err,docs2){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    //console.log(docs2);
+                    res.send("successfully added details");
+                }
+            })
+        })
+})
+    
 router.post("/delete",(req,res)=>{
     let test1 = db.gros;
     let id1=req.body.f2name;
@@ -331,6 +549,48 @@ router.post("/delete",(req,res)=>{
     })
 
 })
+router.post("/Ydelete",(req,res)=>{
+    let test1 = db.gros;
+    let id1=req.body.f22name;
+    let groname=req.body.delname2;
+    //console.log(groname);
+    let f= test1.find({name:id1}).toArray((err,docs1)=>{
+        for(var i=0;i<docs1.length;i++){
+            if(groname == docs1[i].grname.l.prodname){
+                var d= docs1[i]._id;
+                //console.log(docs1[i]);
+                //console.log(d);
+                test1.remove({"_id": d});
+                res.render("Ydelete");
+            }
+            // console.log(docs1[i].grname.l.prodname);
+        }
+        //console.log(docs1[0]);
+    })
+
+})
+
+router.post("/Zdelete",(req,res)=>{
+    let test1 = db.gros;
+    let id1=req.body.f23name;
+    let groname=req.body.delname3;
+    //console.log(groname);
+    let f= test1.find({name:id1}).toArray((err,docs1)=>{
+        for(var i=0;i<docs1.length;i++){
+            if(groname == docs1[i].grname.l.prodname){
+                var d= docs1[i]._id;
+                //console.log(docs1[i]);
+                //console.log(d);
+                test1.remove({"_id": d});
+                res.render("Zdelete");
+            }
+            // console.log(docs1[i].grname.l.prodname);
+        }
+        //console.log(docs1[0]);
+    })
+
+})
+
 
 router.get('/',function(req,res,next){
     res.render('nindex',{title:'Farm to Table'})
